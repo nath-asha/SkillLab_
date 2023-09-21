51bac89fce1185ff4ce117b5bcbec188f9d90934
nath-asha <omthasha@gmail.com>
2023-09-21 17:47:09 +0530

    update

--- site/src/SignupPage.js
+++ site/src/SignupPage.js
@@ -8,5 +8,16 @@ const SignupPage = () => {
     </div>
   );
 }
+<form>
+  <div className="mb-3">
+    <label htmlFor="email" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="email" placeholder="Enter email" />
+  </div>
+  <div className="mb-3">
+    <label htmlFor="password" className="form-label">Password</label>
+    <input type="password" className="form-control" id="password" placeholder="Password" />
+  </div>
+  <button type="submit" className="btn btn-primary">Login</button>
+</form>
 
 export default SignupPage;
