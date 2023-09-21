8ad50b428f8ff67c11f93a654c06da4f66a7aadd
nath-asha <omthasha@gmail.com>
2023-09-21 17:45:17 +0530

    update

---
+++ site/src/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Login</h2>
+      {/* Add your login form here */}
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ site/src/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignupPage = () => {
+  return (
+    <div className="container mt-5">
+      <h2>Sign Up</h2>
+      {/* Add your signup form here */}
+    </div>
+  );
+}
+
+export default SignupPage;
